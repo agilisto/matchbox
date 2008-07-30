121c00c21176d04458734948a961583d530906be
Armand du Plessis <armanddp@agilisto.com>
2008-07-30 19:54:52 +0200

    Added changed ad

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -11,6 +11,7 @@ class Ad
     x = Builder::XmlMarkup.new
     x.ad {
       x.story @story.nil? ? "" : @story.title
+      x.story_link @story.nil? "" : @story.uri
       x.product @product.nil? ? "" : @product.name
       x.score score
       x.ad_copy @product.nil? ? "" : @product.ad_copy
